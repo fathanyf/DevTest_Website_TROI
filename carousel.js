6c4afcadab698cee203fc9f0a45bcc4afb60f032
Fathan Yuda F <fathanupn17@gmail.com>
2023-03-07 12:38:24 +0700

    test gambar revisi

--- carousel.js
+++ carousel.js
@@ -1,24 +1,29 @@
-var slideIndex = 1;
+let slideIndex = 1;
 showSlides(slideIndex);
 
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("slide");
-  if (n > slides.length)
-  {
+  const slides = document.getElementsByClassName("slide");
+  if (n > slides.length) {
     slideIndex = 1;
   }
-  if (n < 1)
-  {
-    slideIndex = slides.length
+  if (n < 1) {
+    slideIndex = slides.length;
   }
-  for (i = 0; i < slides.length; i++)
-  {
+  for (let i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
   }
   slides[slideIndex-1].style.display = "block";
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  setTimeout(showSlides, 3000); // Change image every 3 seconds
 }
